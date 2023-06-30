2c6cbb1d0d6ace3b1c46d9985aea093820b75530
wata-nabee <137441985+wata-nabee@users.noreply.github.com>
2023-07-01 04:18:37 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -72,8 +72,98 @@ $(function(){
       return false;
     });
   });
-  
 
+   jQuery(function() {  //フッターに入ったら消える
+
+    var footer = $('.footer').innerHeight(); // footerの高さを取得
+    
+    window.onscroll = function () {
+      var point = window.pageYOffset; // 現在のスクロール地点 
+      var docHeight = $(document).height(); // ドキュメントの高さ
+      var dispHeight = $(window).height(); // 表示領域の高さ
+    
+      if(point > docHeight-dispHeight-footer){ // スクロール地点>ドキュメントの高さ-表示領域-footerの高さ
+        $('#toTop').addClass('is-hidden'); //footerより下にスクロールしたらis-hiddenを追加
+      }else{
+        $('#toTop').removeClass('is-hidden'); //footerより上にスクロールしたらis-hiddenを削除
+      }
+    };
+  });
+
+ // #page-topをクリックした際の設定
+ $('#page-top').click(function () {
+     $('body,html').animate({
+         scrollTop: 0//ページトップまでスクロール
+     }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+     return false;//リンク自体の無効化
+ });
+
+
+ // スクロールしたら下線
+  jQuery(window).scroll(function() {
+    let scrollTop = jQuery(window).scrollTop(); // スクロール上部の位置
+    let areaTop = jQuery("#concept").offset().top; // 対象エリアの上部の位置
+    let areaBottom = areaTop + jQuery("#concept").innerHeight(); // 対象エリアの下部の位置
+  
+    if (scrollTop > areaTop && scrollTop < areaBottom) {
+      jQuery(".hd-li-1").addClass("active"); // スクロールが対象エリアに入った場合
+    } else {
+      jQuery(".hd-li-1").removeClass("active"); // スクロールが対象エリアから出ている場合
+    }
+
+
+    let areaTop2 = jQuery("#hair-salon").offset().top; // 対象エリアの上部の位置
+    let areaBottom2 = areaTop2 + jQuery("#hair-salon").innerHeight(); // 対象エリアの下部の位置
+  
+    if (scrollTop > areaTop2 && scrollTop < areaBottom2) {
+      jQuery(".hd-li-2").addClass("active"); // スクロールが対象エリアに入った場合
+    } else {
+      jQuery(".hd-li-2").removeClass("active"); // スクロールが対象エリアから出ている場合
+    }
+
+
+    let areaTop3 = jQuery("#self-removal").offset().top; // 対象エリアの上部の位置
+    let areaBottom3 = areaTop3 + jQuery("#self-removal").innerHeight(); // 対象エリアの下部の位置
+  
+    if (scrollTop > areaTop3 && scrollTop < areaBottom3) {
+      jQuery(".hd-li-3").addClass("active"); // スクロールが対象エリアに入った場合
+    } else {
+      jQuery(".hd-li-3").removeClass("active"); // スクロールが対象エリアから出ている場合
+    }
+
+
+    let areaTop4 = jQuery("#cafe").offset().top; // 対象エリアの上部の位置
+    let areaBottom4 = areaTop4 + jQuery("#cafe").innerHeight(); // 対象エリアの下部の位置
+  
+    if (scrollTop > areaTop4 && scrollTop < areaBottom4) {
+      jQuery(".hd-li-4").addClass("active"); // スクロールが対象エリアに入った場合
+    } else {
+      jQuery(".hd-li-4").removeClass("active"); // スクロールが対象エリアから出ている場合
+    }
+
+
+    let areaTop5 = jQuery("#news").offset().top; // 対象エリアの上部の位置
+    let areaBottom5 = areaTop5 + jQuery("#news").innerHeight(); // 対象エリアの下部の位置
+  
+    if (scrollTop > areaTop5 && scrollTop < areaBottom5) {
+      jQuery(".hd-li-5").addClass("active"); // スクロールが対象エリアに入った場合
+    } else {
+      jQuery(".hd-li-5").removeClass("active"); // スクロールが対象エリアから出ている場合
+    }
+
+
+    let areaTop6 = jQuery("#shop-information").offset().top; // 対象エリアの上部の位置
+    let areaBottom6 = areaTop6 + jQuery("#shop-information").innerHeight(); // 対象エリアの下部の位置
+  
+    if (scrollTop > areaTop6 && scrollTop < areaBottom6) {
+      jQuery(".hd-li-6").addClass("active"); // スクロールが対象エリアに入った場合
+    } else {
+      jQuery(".hd-li-6").removeClass("active"); // スクロールが対象エリアから出ている場合
+    }
+  });
+
+
+ 
   // headerのスライダー
   //slickを初期化した際に発生するイベント（初期表示のスライドに.slick-animationのクラスをつける)
   $('#header-slider').on('init', function () {
